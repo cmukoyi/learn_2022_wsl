dca98e8c60a532e3cc77d042357dc48f6c7d2f76
Carlos Mukoyi <carlos@LAPTOP-LOUE78DP.localdomain>
2022-07-16 18:12:09 +0200

    JavaScript lesson Commits

---
+++ javascript/constructor.js
@@ -0,0 +1,18 @@
+
+/* class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+
+    }
+}
+
+let myCar = new Car('Bmw', '745li',2020)
+
+console.log(myCar); */
+
+function MyFunction() {
+
+    
+}
