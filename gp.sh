50b77bfbb907640e8ab67c9e8acd073656955798
Carlos Mukoyi <carlos@LAPTOP-LOUE78DP.localdomain>
2022-07-16 12:19:41 +0200

    [ADD] New gp.sh File

---
+++ gp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+CUR_DIR=$(pwd)
+PROJECT_DIR="${CUR_DIR}"
+
+for fileToCommit in $(find ${PROJECT_DIR}/* -type f);
+do
+  test -f "$fileToCommit" || continue
+  printf "%s\n" "${fileToCommit}"
+  git add "${fileToCommit}"
+  git commit -a -m "[ADD] New ${fileToCommit##*/} File"
+  git push
+done
